bf75f1a41b9432ae14f07ed9d7d158960b7f428e
ShirouzuHaruka <2111140077m@ed.fuk.kindai.ac.jp>
2023-10-22 09:34:06 +0000

    first commit

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,23 @@
+class TopController < ApplicationController
+  def main
+    if session[:login_uid] != nil
+      render "main"
+    else
+      render "login"
+    end
+  end
+
+  def login
+    if User.find_by(uid: params[:uid]) and User.find_by(pass: params[:pass])
+      session[:login_uid] = params[:uid]
+      redirect_to top_main_path
+    else
+      render "error"
+    end
+  end
+  
+  def logout
+    session.delete(:login_uid)
+    redirect_to top_main_path
+  end
+end
